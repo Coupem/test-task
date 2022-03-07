4b607ed559c448f8fa62d417418dcf5881a3f477
Dmitriy.Coupem <dmitriycoupem@gmail.com>
2022-03-07 19:37:30 +0200

    initial material ui library

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import './App.css';
+
+import MainLayout from './components/MainLayout';
 
 function App() {
-  return <div className="App">Learn React</div>;
+  return (
+    <div className="App">
+      <MainLayout />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/MainLayout.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MainLayout = () => {
+  return <div>Hello</div>;
+};
+
+export default MainLayout;
